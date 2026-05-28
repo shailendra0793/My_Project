05e74e1c3abb668ab01e5a886813a5b3e412069b
shailendra0793 <shailendracs.net@gmail.com>
2026-05-28 14:56:14 +0530

    storage

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "rg" {
   name     = "ram"
   location = "West Europe"
 }
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
